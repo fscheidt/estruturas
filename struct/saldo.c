ecd9450c314fe4ac7190f6557bf5754de8da8bc4
Felippe Scheidt <felippescheidt@gmail.com>
2026-08-14 20:33:14 -0300

    add saldo

--- struct/saldo.c
+++ struct/saldo.c
@@ -5,18 +5,22 @@ typedef struct {
   float saldo;
   char chave[50];
 } Cliente;
+
 void extrato(Cliente cliente){
   printf("\n[extrato] %s (saldo=R$ %.2f)",cliente.nome, cliente.saldo);
 }
-bool transfere(){
-  /*
-  if(VALIDAR){
+bool transfere(Cliente *origem, Cliente *destino, float valor){
+  
+  if(origem->saldo < valor){
     printf("\n\t[pix] saldo insuficiente para realizar o pix R$ %.2f", valor);
     return false;
   } 
+  
+  origem->saldo -= valor;
+  destino->saldo += valor;
   // atualizar saldos
   printf("\n\t[pix] valor de R$ %.2f enviado", valor);
-  */
+  
   return true;
 }
 int main() {
@@ -25,15 +29,18 @@ int main() {
   float valor = 300;
   extrato(maria); // 1200
   extrato(joao);  // 800
-  //transfere();
+  transfere(&joao, &maria, valor);
+
   extrato(maria); // 1500
   extrato(joao);  // 500
   valor = 600;
-  //transfere();
+  transfere(&joao, &maria, valor);
+
   extrato(maria); // 1500
   extrato(joao);  // 500
   valor = 500;
-  //transfere();
-  extrato(maria); // ?
-  extrato(joao);  // ?
+  transfere(&joao, &maria, valor);
+
+  extrato(maria); // 2000
+  extrato(joao);  // 0
 }
